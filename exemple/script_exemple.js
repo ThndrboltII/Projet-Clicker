8e855dfda735e58574ae7b4175f207fca9607796
ThndrboltII <lallemant.flavien@gmail.com>
2019-03-01 10:25:41 +0100

    rbz

--- exemple/script_exemple.js
+++ exemple/script_exemple.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var compte = 0;
   $('.case').on("click",function(){
-    compte = compte + 1;
+    compte = (compte * 2) + 1;
     $('#compteur').text('Compteur: ' + compte);
   });
 });
