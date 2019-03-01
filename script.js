bc39cabef958facc5e653e71b9e3734d43415668
ThndrboltII <lallemant.flavien@gmail.com>
2019-03-01 09:11:55 +0100

    Good

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
   var compte = 0;
-  var compteur = document.getElementById("compteur");
-  $('case').on("click",function(){
+  $('.case').on("click",function(){
     compte = compte + 1;
-    compteur.innerHTML = "compteur: " + compte
+    $('#compteur').text('Compteur: ' + compte);
   });
 });
