d3352e085fb9cd386e54e182cf5b1deabb431f92
ThndrboltII <lallemant.flavien@gmail.com>
2019-03-15 23:13:08 +0100

    proba minerais

--- script/app1.js
+++ script/app1.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
   var compte = 0;
   $('map').on("click",function(){
-    compte = compte + 1;
     $('#compteur').text('Compteur: ' + compte);
+
+    alea();
+
+    function alea(){
+      var rockAlea = Math.random();
+        if(rockAlea <= item[0].rare){
+          compte = compte + parseInt(item[0].value);
+        };
+      var goldAlea = Math.random();
+        if(goldAlea <= item[1].rare){
+          compte = compte + parseInt(item[1].value);
+        };
+    };
   });
 
-  var item = [{item:"ROCK", value:"1", rare:"0.98"},{item:"GOLD", value:"10", rare:"0.3"},{item:"DIAMOND", value:"500", rare:"0.1"},{item:"PALLADIUM", value:"700", rare:"0.05"},
-  {item:"URANIUM", value:"1000", rare:"0.01"},{item:"EMERALD", value:"200", rare:"0.4"},{item:"RUBY", value:"200", rare:"0.4"},{item:"SAPPHIRE", value:"200", rare:"0.4"}];
+  var item = [{item:"ROCK", value:"1", rare:"0.98"},{item:"GOLD", value:"10", rare:"0.3"},{item:"DIAMOND", value:"500", rare:"0.1"},{item:"PALLADIUM", value:"700", rare:"0.05"},{item:"URANIUM", value:"1000", rare:"0.01"},{item:"EMERALD", value:"200", rare:"0.4"},{item:"RUBY", value:"200", rare:"0.4"},{item:"SAPPHIRE", value:"200", rare:"0.4"}];
 });
