5a28a712720599eade5abe3fcbb44c83fc8c835d
ThndrboltII <lallemant.flavien@gmail.com>
2019-03-15 10:25:06 +0100

    ITEMS

--- script/app1.js
+++ script/app1.js
@@ -5,5 +5,6 @@ $(document).ready(function(){
     $('#compteur').text('Compteur: ' + compte);
   });
 
-  var item = [];
+  var item = [{item:"ROCK", value:"1", rare:"0.98"},{item:"GOLD", value:"10", rare:"0.3"},{item:"DIAMOND", value:"500", rare:"0.1"},{item:"PALLADIUM", value:"700", rare:"0.05"},
+  {item:"URANIUM", value:"1000", rare:"0.01"},{item:"EMERALD", value:"200", rare:"0.4"},{item:"RUBY", value:"200", rare:"0.4"},{item:"SAPPHIRE", value:"200", rare:"0.4"},];
 });
