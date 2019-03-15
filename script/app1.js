9dae86f633b0cf0ce4bf6dfbc33035f1cd2a726b
ThndrboltII <lallemant.flavien@gmail.com>
2019-03-15 22:27:07 +0100

    Nothing have changed

--- script/app1.js
+++ script/app1.js
@@ -6,5 +6,5 @@ $(document).ready(function(){
   });
 
   var item = [{item:"ROCK", value:"1", rare:"0.98"},{item:"GOLD", value:"10", rare:"0.3"},{item:"DIAMOND", value:"500", rare:"0.1"},{item:"PALLADIUM", value:"700", rare:"0.05"},
-  {item:"URANIUM", value:"1000", rare:"0.01"},{item:"EMERALD", value:"200", rare:"0.4"},{item:"RUBY", value:"200", rare:"0.4"},{item:"SAPPHIRE", value:"200", rare:"0.4"},];
+  {item:"URANIUM", value:"1000", rare:"0.01"},{item:"EMERALD", value:"200", rare:"0.4"},{item:"RUBY", value:"200", rare:"0.4"},{item:"SAPPHIRE", value:"200", rare:"0.4"}];
 });
