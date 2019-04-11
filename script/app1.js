f88f83e1ad4ddaba4e10512f69372828bea7ca8d
ThndrboltII <lallemant.flavien@gmail.com>
2019-04-11 19:40:14 +0200

    Probas différentes pour chaque niveau.

--- script/app1.js
+++ script/app1.js
@@ -1,48 +1,230 @@
 $(document).ready(function(){
+  var alea;
   $('img[usemap]').rwdImageMaps();
   $('#left_arrow').css('visibility', 'hidden');
   $('map').on("click",function(){
 
-    alea();
+    falea();
 
-    function alea(){
-      var alea = Math.random().toFixed(4);
+    function falea(){
+      alea = Math.random().toFixed(4);
+      backgroundMineral();
     };
 
-    // Set rock counter
-    if(alea>=0 && alea<=0.7434){
-      item[0].counter = parseInt(item[0].counter) + 1;
-      $("#rock").text("Rock: " + item[0].counter);
-    };
+    function log(){
+      console.log(alea);
+    }
 
-    // Set coal counter
-    if(alea>=0.7434 && alea<=0.9234){
-      item[1].counter = parseInt(item[1].counter) + 1;
-      $("#coal").text("Coal: " + item[1].counter);
-    };
+    function backgroundMineral(){
+      switch ($('img').attr('src')) {
+        case '../background/PNG/game_background_2/game_background_2.png':
+          // Set rock counter
+          if(alea>=0 && alea<=0.7434){
+            item[0].counter = parseInt(item[0].counter) + 1;
+            $("#rock").text("Rock: " + item[0].counter);
+          };
 
-    // Set gold counter
-    if(alea>=0.9234 && alea<=0.9474){
-      item[2].counter = parseInt(item[2].counter) + 1;
-      $("#gold").text("Gold: " + item[2].counter);
-    };
+          // Set coal counter
+          if(alea>=0.7434 && alea<=0.9234){
+            item[1].counter = parseInt(item[1].counter) + 1;
+            $("#coal").text("Coal: " + item[1].counter);
+          };
 
-    // Set diamond counter
-    if(alea>=0.9474 && alea<=0.9494){
-      item[3].counter = parseInt(item[3].counter) + 1;
-      $("#diamond").text("Diamond: " + item[3].counter);
-    };
+          // Set gold counter
+          if(alea>=0.9234 && alea<=0.9474){
+            item[2].counter = parseInt(item[2].counter) + 1;
+            $("#gold").text("Gold: " + item[2].counter);
+          };
 
-    // Set palladium counter
-    if(alea>=0.9494 && alea<=0.95){
-      item[4].counter = parseInt(item[4].counter) + 1;
-      $("#palladium").text("Palladium: " + item[4].counter);
-    };
+          // Set diamond counter
+          if(alea>=0.9474 && alea<=0.9494){
+            item[3].counter = parseInt(item[3].counter) + 1;
+            $("#diamond").text("Diamond: " + item[3].counter);
+          };
 
-    // Set lapis-lazuli counter
-    if(alea>=0.95 && alea<=1){
-      item[9].counter = parseInt(item[9].counter) + 1;
-      $("#lapis").text("Lapis-Lazuli: " + item[9].counter);
+          // Set palladium counter
+          if(alea>=0.9494 && alea<=0.95){
+            item[4].counter = parseInt(item[4].counter) + 1;
+            $("#palladium").text("Palladium: " + item[4].counter);
+          };
+
+          // Set lapis-lazuli counter
+          if(alea>=0.95 && alea<=1){
+            item[9].counter = parseInt(item[9].counter) + 1;
+            $("#lapis").text("Lapis-Lazuli: " + item[9].counter);
+          };
+          break;
+
+        case '../background/PNG/mine.png':
+          // Set rock counter
+          if(alea>=0 && alea<=0.4974){
+            item[0].counter = parseInt(item[0].counter) + 1;
+            $("#rock").text("Rock: " + item[0].counter);
+          };
+
+          // Set coal counter
+          if(alea>=0.4974 && alea<=0.8974){
+            item[1].counter = parseInt(item[1].counter) + 1;
+            $("#coal").text("Coal: " + item[1].counter);
+          };
+
+          // Set gold counter
+          if(alea>=0.8974 && alea<=0.9474){
+            item[2].counter = parseInt(item[2].counter) + 1;
+            $("#gold").text("Gold: " + item[2].counter);
+          };
+
+          // Set diamond counter
+          if(alea>=0.9474 && alea<=0.9494){
+            item[3].counter = parseInt(item[3].counter) + 1;
+            $("#diamond").text("Diamond: " + item[3].counter);
+          };
+
+          // Set palladium counter
+          if(alea>=0.9494 && alea<=0.95){
+            item[4].counter = parseInt(item[4].counter) + 1;
+            $("#palladium").text("Palladium: " + item[4].counter);
+          };
+
+          // Set lapis-lazuli counter
+          if(alea>=0.95 && alea<=1){
+            item[9].counter = parseInt(item[9].counter) + 1;
+            $("#lapis").text("Lapis-Lazuli: " + item[9].counter);
+          };
+          break;
+
+        case '../background/PNG/game_background_1/game_background_1.png':
+          // Set rock counter
+          if(alea>=0 && alea<=0.5374){
+            item[0].counter = parseInt(item[0].counter) + 1;
+            $("#rock").text("Rock: " + item[0].counter);
+          };
+
+          // Set coal counter
+          if(alea>=0.5374 && alea<=0.6674){
+            item[1].counter = parseInt(item[1].counter) + 1;
+            $("#coal").text("Coal: " + item[1].counter);
+          };
+
+          // Set gold counter
+          if(alea>=0.6674 && alea<=0.6774){
+            item[2].counter = parseInt(item[2].counter) + 1;
+            $("#gold").text("Gold: " + item[2].counter);
+          };
+
+          // Set diamond counter
+          if(alea>=0.6774 && alea<=0.6794){
+            item[3].counter = parseInt(item[3].counter) + 1;
+            $("#diamond").text("Diamond: " + item[3].counter);
+          };
+
+          // Set palladium counter
+          if(alea>=0.6794 && alea<=0.68){
+            item[4].counter = parseInt(item[4].counter) + 1;
+            $("#palladium").text("Palladium: " + item[4].counter);
+          };
+
+          // Set emerald counter
+          if(alea>=0.68 && alea<=0.76){
+            item[6].counter = parseInt(item[6].counter) + 1;
+            $("#emerald").text("Emerald: " + item[6].counter);
+          };
+
+          // Set ruby counter
+          if(alea>=0.76 && alea<=0.84){
+            item[7].counter = parseInt(item[7].counter) + 1;
+            $("#ruby").text("Ruby: " + item[7].counter);
+          };
+
+          // Set sapphire counter
+          if(alea>=0.84 && alea<=0.92){
+            item[8].counter = parseInt(item[8].counter) + 1;
+            $("#sapphire").text("Sapphire: " + item[8].counter);
+          };
+
+          // Set lapis-lazuli counter
+          if(alea>=0.92 && alea<=1){
+            item[9].counter = parseInt(item[9].counter) + 1;
+            $("#lapis").text("Lapis-Lazuli: " + item[9].counter);
+          };
+          break;
+
+        case '../background/PNG/game_background_4/game_background_4.png':
+          // Set rock counter
+          if(alea>=0 && alea<=0.293){
+            item[0].counter = parseInt(item[0].counter) + 1;
+            $("#rock").text("Rock: " + item[0].counter);
+          };
+
+          // Set coal counter
+          if(alea>=0.293 && alea<=0.393){
+            item[1].counter = parseInt(item[1].counter) + 1;
+            $("#coal").text("Coal: " + item[1].counter);
+          };
+
+          // Set gold counter
+          if(alea>=0.393 && alea<=0.427){
+            item[2].counter = parseInt(item[2].counter) + 1;
+            $("#gold").text("Gold: " + item[2].counter);
+          };
+
+          // Set diamond counter
+          if(alea>=0.427 && alea<=0.437){
+            item[3].counter = parseInt(item[3].counter) + 1;
+            $("#diamond").text("Diamond: " + item[3].counter);
+          };
+
+          // Set palladium counter
+          if(alea>=0.437 && alea<=0.44){
+            item[4].counter = parseInt(item[4].counter) + 1;
+            $("#palladium").text("Palladium: " + item[4].counter);
+          };
+
+          // Set emerald counter
+          if(alea>=0.44 && alea<=0.56){
+            item[6].counter = parseInt(item[6].counter) + 1;
+            $("#emerald").text("Emerald: " + item[6].counter);
+          };
+
+          // Set ruby counter
+          if(alea>=0.56 && alea<=0.68){
+            item[7].counter = parseInt(item[7].counter) + 1;
+            $("#ruby").text("Ruby: " + item[7].counter);
+          };
+
+          // Set sapphire counter
+          if(alea>=0.68 && alea<=0.8){
+            item[8].counter = parseInt(item[8].counter) + 1;
+            $("#sapphire").text("Sapphire: " + item[8].counter);
+          };
+
+          // Set lapis-lazuli counter
+          if(alea>=0.8 && alea<=1){
+            item[9].counter = parseInt(item[9].counter) + 1;
+            $("#lapis").text("Lapis-Lazuli: " + item[9].counter);
+          };
+          break;
+
+        case '../background/PNG/nuclear_plant.png':
+          // Set rock counter
+          if(alea>=0 && alea<=0.813){
+            item[0].counter = parseInt(item[0].counter) + 1;
+            $("#rock").text("Rock: " + item[0].counter);
+          };
+
+          // Set gold counter
+          if(alea>=0.813 && alea<=0.83){
+            item[2].counter = parseInt(item[2].counter) + 1;
+            $("#gold").text("Gold: " + item[2].counter);
+          };
+
+          // Set uranium counter
+          if(alea>=0.83 && alea<=1){
+            item[5].counter = parseInt(item[5].counter) + 1;
+            $("#uranium").text("Uranium: " + item[5].counter);
+          };
+          break;
+      };
     };
   });
 
@@ -52,9 +234,9 @@ $(document).ready(function(){
   {item:"DIAMOND", value:"500", counter:"0"},
   {item:"PALLADIUM", value:"700", counter:"0"},
   {item:"URANIUM", value:"1000", counter:"0"},
-  {item:"EMERALD", value:"200", counter:"0"},
-  {item:"RUBY", value:"200", counter:"0"},
-  {item:"SAPPHIRE", value:"200", counter:"0"},
+  {item:"EMERALD", value:"100", counter:"0"},
+  {item:"RUBY", value:"100", counter:"0"},
+  {item:"SAPPHIRE", value:"100", counter:"0"},
   {item:"LAPIS-LAZULI", value:"10", counter:"0"}];
 
   $('#left_arrow').on("click", function(){
