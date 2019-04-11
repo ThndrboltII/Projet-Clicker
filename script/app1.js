abdb448f901712766dc1c2d6e509fb8bf6969b79
ThndrboltII <lallemant.flavien@gmail.com>
2019-04-11 22:19:58 +0200

    SHREK

--- script/app1.js
+++ script/app1.js
@@ -296,4 +296,18 @@ $(document).ready(function(){
         break;
     }
   });
+
+  //https://gist.github.com/MTDdk/2aaa51d1e8cc2e6bcf1d30c2f205afe6
+  var audio = document.querySelector('audio');
+  if (audio) {
+    window.addEventListener('keydown', function (event) {
+      var key = event.which || event.keyCode;
+      if (key === 32) { // spacebar
+        // eat the spacebar, so it does not scroll the page
+        event.preventDefault();
+        audio.paused ? audio.play() : audio.pause();
+      }
+    });
+  }
+
 });
