cc3117950b1a6ee4e8305dac0ec31daf88f0a2a4
Bastien Cortequisse <cortequisse.bastien@gmail.com>
2019-05-03 10:25:17 +0200

    Thanos

--- ExFeuille sprite Batman/projet.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var mc = new JSMovieclip($('#my-element'),{
-    framerate : 6,
-    frames :[{x:0,y:0},{x:101,y:0},{x:202,y:0},{x:303,y:0},{x:0,y:100},{x:101,y:100},{x:202,y:100},{x:303,y:100},{x:0,y:200},{x:101,y:200},{x:202,y:200},{x:303,y:200},{x:0,y:300},{x:101,y:300}]
-});	
-
-mc.play(true);
-
-});
